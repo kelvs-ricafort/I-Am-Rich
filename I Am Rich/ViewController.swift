c23159a1ee5e5189a7cf9497224c17f36a7d833b
Kelvin C. Ricafort <kelvs.r@icloud.com>
2023-07-11 14:46:01 +0800

    With Constraints

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Kelvin Ricafort on 7/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
